b5cce0a4362dd04871aca94f5aa56b225c6f4a30
John Anderson <lampwins@gmail.com>
2017-02-20 03:44:18 -0500

    okay, working now

--- api/models.py
+++ api/models.py
@@ -52,7 +52,7 @@ class User(Base):
         """
         try:
             payload = {
-                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1, seconds=0),
                 'iat': datetime.datetime.utcnow(),
                 'sub': user_id
             }
