0df3d85fe05c1a851ca1ee77701751fe4733b9d0
Miki Burleson Sugimoto <mikisugimoto@mikis-air.wireless.cougars.int>
2017-04-04 17:58:14 -0400

    Added change request page 1

---
+++ frontend/blur-admin/src/app/pages/changeRequest/summary/summaryCtrl.js
@@ -0,0 +1,29 @@
+/**
+ * @author v.lugovksy
+ * created on 16.12.2015
+ */
+(function () {
+  'use strict';
+
+  angular.module('BlurAdmin.pages.changeRequest')
+      .controller('SummaryCtrl', SummaryCtrl);
+
+
+  /** @ngInject */
+  function SummaryCtrl($scope, baConfig, $http, $auth, $location) {
+
+
+    $http({
+      method : "GET",
+      url : "/api/v1.0/change_requests/"
+    }).then(function mySuccess(response){
+      console.log(response)
+      $scope.queueData = response.data.data;
+    }, function myError(response) {
+      console.log(response)
+      $scope.name = response.statusText;
+    });
+
+
+}
+})();
