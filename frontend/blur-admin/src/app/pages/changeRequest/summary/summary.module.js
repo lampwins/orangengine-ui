9e4b9af4c61d2761f93d0809d49fe2447de047fe
Miki Burleson Sugimoto <mikisugimoto@mikis-air.wireless.cougars.int>
2017-04-06 18:47:47 -0400

    Summary fixes and broken page

---
+++ frontend/blur-admin/src/app/pages/changeRequest/summary/summary.module.js
@@ -0,0 +1,17 @@
+(function () {
+  'use strict';
+
+  angular.module('BlurAdmin.pages.changeRequest.summary', [])
+    .config(routeConfig);
+
+  /** @ngInject */
+  function routeConfig($stateProvider) {
+    $stateProvider
+      .state('changeRequest.summary', {
+        url: '/:id/summary',
+        templateUrl: 'app/pages/changeRequest/summary/summary.html',
+          title: 'Summary',
+          controller: "SummaryCtrl"
+      });
+  }
+})();
