2eead50ade9a073b665d19081621919994fb9155
Miki Burleson Sugimoto <mikisugimoto@Mikis-MacBook-Air.local>
2017-04-17 21:49:42 -0400

    Finished environment

--- frontend/blur-admin/src/app/pages/changeRequest/environment/environmentCtrl.js
+++ frontend/blur-admin/src/app/pages/changeRequest/environment/environmentCtrl.js
@@ -5,7 +5,7 @@
       .controller('EnvironmentCtrl', EnvironmentCtrl);
 
   /** @ngInject */
-  function EnvironmentCtrl($scope) {
+  function EnvironmentCtrl($scope, $http) {
 
     var vm = this;
     vm.disabled = undefined;
@@ -14,6 +14,17 @@
       {label: 'Juniper SRX', value: 'juniper_srx'},
       {label: 'Palo Alto Panorama', value: 'palo_alto_panorama'},
     ];
+
+    $http({
+      method : "GET",
+      url : "/api/v1.0/change_requests/"
+    }).then(function mySuccess(response){
+      console.log(response)
+      $scope.environmentData = response.data.data;
+    }, function myError(response) {
+      console.log(response)
+      $scope.name = response.statusText;
+    });
 
 
 
